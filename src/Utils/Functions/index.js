4d64c28641f825ad3e66e226d28656a3438eea9a
peyman2dev <peyman2dev@gmail.com>
2024-04-07 17:01:30 +0330

    Navbar items added

--- src/Utils/Functions/index.js
+++ src/Utils/Functions/index.js
@@ -8,13 +8,13 @@ import {
   toggleThemeTo,
 } from "./theme.js";
 
-import { profileButton,profileDropdown, toggleProfile } from "./profile.js";
+import { profileButton, profileDropdown, toggleProfile } from "./profile.js";
 
 const $ = document;
-const navbarScreen = $.querySelector("#navbar-wrap")
-const navbar = $.querySelector("#navbar")
-const navbarButton = $.querySelector("#navbar-button")
-const navbarCloseBtn = $.querySelector("#close-navbar")
+const navbarScreen = $.querySelector("#navbar-wrap");
+const navbar = $.querySelector("#navbar");
+const navbarButton = $.querySelector("#navbar-button");
+const navbarCloseBtn = $.querySelector("#close-navbar");
 
 //  Theme Handler
 themeButton.addEventListener("click", (event) => {
@@ -43,36 +43,34 @@ systemButton.addEventListener("click", (event) => {
 });
 
 // Profile dropdown
-profileButton.addEventListener('click', event => {
-  event.stopPropagation()
-  toggleProfile()
-})
+profileButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  toggleProfile();
+});
 
 const toggleNav = () => {
-  navbarScreen.classList.toggle('invisible')
-  navbarScreen.classList.toggle('opacity-0')
-  navbar.classList.toggle('right-[-1440px]')
-  navbar.classList.toggle('right-[0]')
-}
+  navbarScreen.classList.toggle("invisible");
+  navbarScreen.classList.toggle("opacity-0");
+  navbar.classList.toggle("right-[-1440px]");
+  navbar.classList.toggle("right-[0]");
+};
 
-
-navbarButton.addEventListener('click', event => {
-  if (navbarScreen.classList.contains('opacity-0') && navbarScreen.classList.contains('invisible')) {
-    toggleNav()
+navbarButton.addEventListener("click", (event) => {
+  if (
+    navbarScreen.classList.contains("opacity-0") &&
+    navbarScreen.classList.contains("invisible")
+  ) {
+    toggleNav();
   }
-})
-navbar.addEventListener('click', event => {
-  event.stopPropagation()
-})
+});
+navbar.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
 
-navbarScreen.addEventListener('click', () => {
-  toggleNav()
-})
+navbarScreen.addEventListener("click", () => toggleNav());
+navbarCloseBtn.addEventListener("click", () => toggleNav());
 
-
-navbarCloseBtn.addEventListener("click", () => toggleNav())
-
-//  mounted actions 
+//  mounted actions
 window.onload = () => {
   const localTheme = localStorage.getItem("theme");
   if (localTheme && localTheme === "light") {
@@ -81,7 +79,6 @@ window.onload = () => {
     document.documentElement.className = "dark";
   }
 };
-
 
 document.addEventListener("click", (event) => {
   if (
@@ -96,7 +93,7 @@ document.addEventListener("click", (event) => {
     !profileDropdown.classList.contains("opacity-0") &&
     !profileDropdown.classList.contains("invisible")
   ) {
-      profileDropdown.classList.toggle("opacity-0");
-      profileDropdown.classList.toggle("invisible");
+    profileDropdown.classList.toggle("opacity-0");
+    profileDropdown.classList.toggle("invisible");
   }
 });
