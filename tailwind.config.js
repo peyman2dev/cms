6f2e4b1fc768dbc8cdd892a1a9cd5e367f6dc2e7
peyman2dev <peyman2dev@gmail.com>
2024-04-07 13:02:50 +0330

    CMS Init

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      
+      fontFamily: {
+        "Geist-Light": "Geist-Light",
+        "Geist-Regular": "Geist-Regular",
+        "Geist-Medium": "Geist-Medium",
+        "Geist-SemiBold": "Geist-SemiBold",
+        "Geist-Bold": "Geist-Bold",
+        "Geist-Black": "Geist-Black",
+        "Geist-UltraBlack": "Geist-UltraBlack",
+      },
+    },
+  },
+  plugins: [function({addVariant}) {
+    addVariant("child", "& > *")
+    addVariant("child-hover", "& > *:hover")
+  }],
+};
